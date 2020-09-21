7145a384dd656c4a3a64ad9407849489799902eb
abdulbasit <mrspeed518@gmail.com>
2020-09-21 10:53:30 +0100

    fixing layout

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 let app = document.getElementById('app');
 let toggleBtn = document.querySelector('.btn');
 
-document.getElementById('app').addEventListener('click', (e) => {
+document.querySelector('.nav-btn').addEventListener('click', (e) => {
+    // e.preventDefault();
+    document.querySelector('.app').classList.toggle('show-nav');
+});
+document.querySelector('.close-nav').addEventListener('click', (e) => {
     // e.preventDefault();
     document.querySelector('.app').classList.toggle('show-nav');
 });
