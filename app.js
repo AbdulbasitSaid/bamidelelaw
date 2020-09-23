524441fab590c6da93a314038982f608ed7f449e
abdulbasit <mrspeed518@gmail.com>
2020-09-23 04:18:22 +0100

    updates

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ let app = document.querySelector('.app');
 let toggleBtn = document.querySelector('.nav-btn');
 let sideNav = document.querySelector('.side-nav');
 let closeBtn = document.querySelector('.close-nav');
+let loader = document.querySelector('.loader');
 
+window.onload = () => {
+    setInterval(() => {
+        loader.classList.add('show');
+    }, 1000)
+    loader.classList.add('hide');
+}
 toggleBtn.addEventListener('click', (e) => {
     // e.preventDefault();
     app.classList.toggle('show-nav');
@@ -12,3 +19,4 @@ closeBtn.addEventListener('click', (e) => {
     // e.preventDefault();
     document.querySelector('.app').classList.toggle('show-nav');
 });
+
