0e820e05309ac045880383a0e5c237f3756d812a
abdulbasit <mrspeed518@gmail.com>
2020-09-24 12:29:14 +0100

    updates

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ let navbar = document.querySelector('.navbar');
 
 app.classList.add('hide');
 sideNav.classList.add('hide');
+navbar.classList.add('hide');
 setTimeout(() => {
     loader.classList.add('hide');
     app.classList.remove('hide');
     sideNav.classList.remove('hide');
-
+    navbar.classList.remove('hide');
 }, 1500);
 
 
@@ -20,7 +21,7 @@ toggleBtn.addEventListener('click', (e) => {
     app.classList.toggle('show-nav');
     navbar.classList.toggle('show-nav')
 
-    // sideNav.classList.toggle('show');
+    sideNav.classList.toggle('show');
 });
 closeBtn.addEventListener('click', (e) => {
     // e.preventDefault();
