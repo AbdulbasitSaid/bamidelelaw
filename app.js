07dfb43b61c2dabfc2d917392e0c039c878fcfc3
abdulbasit <mrspeed518@gmail.com>
2020-09-25 02:30:25 +0100

    updates

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 let app = document.querySelector('.app');
-let toggleBtn = document.querySelector('.nav-btn');
-let sideNav = document.querySelector('.side-nav');
-let closeBtn = document.querySelector('.close-nav');
-let loader = document.querySelector('.loader3');
-let navbar = document.querySelector('.navbar');
+let toggleBtn = document.querySelector('.nav-btn')
+let sideNav = document.querySelector('.side-nav')
+let closeBtn = document.querySelector('.close-nav')
+let loader = document.querySelector('.loader3')
+let navbar = document.querySelector('.navbar')
+//
+let navItems = document.getElementById('nav-items')
+let navlinks = navItems.getElementsByClassName('link')
+
+
+for (let i = 0; i < navlinks.length; i++) {
+    navlinks[i].addEventListener('click', () => {
+        let current = document.getElementsByClassName('current')
+        console.log(current)
+        console.log('greater')
+        current[0].className = current[0].className.replace('current', ' ')
+        this.className += 'current'
+    })
+}
 
 app.classList.add('hide');
-sideNav.classList.add('hide');
-navbar.classList.add('hide');
+sideNav.classList.add('hide')
+navbar.classList.add('hide')
+
 setTimeout(() => {
     loader.classList.add('hide');
     app.classList.remove('hide');
